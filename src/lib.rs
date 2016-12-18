b2f432043fbf10a428238dc3e1bee06b44f55617
nathanfaucett <nathanfaucett@gmail.com>
2016-12-18 11:09:50 -0600

    clean up

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ pub mod x86_64;
 
 
 #[cfg(target_arch = "x86")]
-pub use x86::{IdtDescriptor, IdtEntry};
+pub use x86::*;
 
 #[cfg(target_arch = "x86_64")]
-pub use x86_64::{IdtDescriptor, IdtEntry};
+pub use x86_64::*;
